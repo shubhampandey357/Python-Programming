e95f0135d04b4b2f4d5c398fa180c3532e66da29
shubhampandey357 <106475377+shubhampandey357@users.noreply.github.com>
2022-05-30 19:09:30 +0530

    Add files via upload

---
+++ numpy/numpy3.py
@@ -0,0 +1,18 @@
+# Program to add two matrices.
+import numpy as np  
+n=int(input())
+m=int(input())
+a=[]
+b=[]
+for i in range(n):
+    l1=list(map(int,input().split()))
+    a.append(l1)
+for i in range(n):
+    l2=list(map(int,input().split()))
+    b.append(l2)
+x=np.array(a)
+y=np.array(b)
+x.shape=(n,m)
+y.shape=(n,m)
+c=np.add(x,y)
+print(c)
