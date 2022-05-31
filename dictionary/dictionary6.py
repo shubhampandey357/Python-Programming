5ddfee7a1c48a0c8060806ebd3aa867f5a4ad868
Shubham Pandey <106475377+shubhampandey357@users.noreply.github.com>
2022-05-31 10:31:33 +0530

    Add files via upload

---
+++ dictionary/dictionary6.py
@@ -0,0 +1,11 @@
+a={}
+size=int(input("enter the size"))
+for i in range(size):
+  key=int(input("enter the key"))
+  value=int(input("enter the value"))
+  a[key]=value
+print(a)
+k=1
+for j in a.value:
+  k=k*j
+print(k)
