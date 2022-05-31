07c15b312de60fcb5669812738446869d6ba50e6
Shubham Pandey <106475377+shubhampandey357@users.noreply.github.com>
2022-05-31 10:54:49 +0530

    Add files via upload

---
+++ miscellaneous/random codes/random5.py
@@ -0,0 +1,9 @@
+sample_list = [1, 2, 3, 4, 5]
+print("Original list : ")
+print(sample_list)
+random.shuffle(sample_list)
+print("\nAfter the first shuffle : ")
+print(sample_list)
+random.shuffle(sample_list)
+print("\nAfter the second shuffle : ")
+print(sample_list)
