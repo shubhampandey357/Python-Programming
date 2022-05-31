fc7623b0402ca047fbfa4597ee076dd3e0408754
Shubham Pandey <106475377+shubhampandey357@users.noreply.github.com>
2022-05-31 10:08:27 +0530

    Add files via upload

---
+++ numpy/numpy7.py
@@ -0,0 +1,18 @@
+#program to divide two matrix
+import numpy as np  
+n=int(input())
+m=int(input())
+a=[]
+b=[]
+for i in range(n):
+    l1=list(map(int,input().split()))
+    a.append(l1)
+for i in range(n):
+    l2=list(map(int,input().split()))
+    b.append(l2)
+x=np.array(a)
+y=np.array(b)
+x.shape=(n,m)
+y.shape=(n,m)
+c=np.divide(x,y)
+print(c)
